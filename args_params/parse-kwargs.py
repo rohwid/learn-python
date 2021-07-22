d0f016afe566bdce23e88a2279c39548bc957def
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-07-22 14:15:30 +0700

    parese from kwrags func to other kwargs func, and init var iniside if

--- args_params/parse-kwargs.py
+++ args_params/parse-kwargs.py
@@ -1,7 +1,13 @@
+def parse_kwargs(**kwargs):
+    print('kwargs2: ', kwargs)
+    print('named2: ', kwargs['name'])
+    print('age2: ', kwargs['age'])
+
 def named(**kwargs):
-    print('kwargs: ', kwargs)
-    print('named: ', kwargs['name'])
-    print('age: ', kwargs['age'])
+    print('kwargs1: ', kwargs)
+    print('named1: ', kwargs['name'])
+    print('age1: ', kwargs['age'])
+    parse_kwargs(**kwargs)
 
 named(name="Bob", age=25)
 
---
+++ decision/var-inside-if.py
@@ -0,0 +1,8 @@
+a = 10
+
+if a == 10:
+    b = a
+else:
+    b = 0
+
+print(b)
