f180b3ceae1b1cac03262ea5b6c615d9b04503bc
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-09-14 14:15:22 +0700

    fix with_decorator and parse param in decorator

--- decorators/with_decorator.py
+++ decorators/with_decorator.py
@@ -4,19 +4,28 @@ user = {"username": "jose", "access_level": "guest"}
 
 def make_secure(func):
     @functools.wraps(func)
-    def secure_function():
+    def secure_function(*args, **kwargs):
         if user["access_level"] == "admin":
-            return func()
+            return func(*args, **kwargs)
+        else:
+            return f"No admin permission for {user['username']}."
     
     return secure_function
 
 @make_secure
-def get_admin_password():
-    return "1234"
+def get_password(panel): # get_password gonna replace with secure_function
+    if panel == "admin": # so this condition gonna be ignore
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
 
-print(get_admin_password.__name__)
-print(get_admin_password())
+print(get_password.__name__)
+print(get_password("billing"))
 
 user = {"username": "jose", "access_level": "admin"}
-print(get_admin_password.__name__)
-print(get_admin_password())
+print(get_password.__name__)
+print(get_password("admin"))
+
+user = {"username": "anna", "access_level": "admin"}
+print(get_password.__name__)
+print(get_password("billing"))
