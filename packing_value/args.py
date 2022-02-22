a8660d48420f0918de9e2431914ccd459411d0a5
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2022-02-22 09:20:37 +0700

    packing and unpacking values

--- packing_value/args.py
+++ packing_value/args.py
@@ -4,6 +4,9 @@ def print_array(a, b, c):
 array = [1, 2, 3]
 parse_array = [*array]
 
+print_array(array[0], array[1], array[2])
+
+# Simplified
 print(*array)
 print_array(*array)
 
--- packing_value/kwargs.py
+++ packing_value/kwargs.py
@@ -3,4 +3,7 @@ def print_struct(a, b, c):
 
 struct = {'a': 10, 'b':20, 'c': 30}
 
+print_struct(struct['a'], struct['b'], struct['c'])
+
+# Simplified
 print_struct(**struct)
