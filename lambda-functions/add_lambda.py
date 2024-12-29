73ac8854b95df367523ea544dfb45607d2ad498a
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2024-12-29 13:17:20 +0700

    feat: more lambda function example

--- lambda-functions/add_lambda.py
+++ lambda-functions/add_lambda.py
@@ -1,8 +1,13 @@
-# variable = lambda param1, param2: return value
-add = lambda x, y: x + y
+import datetime
 
+# function_name = lambda param1, param2: return value
+add = lambda x, y: x + y
 print(add(5, 7))
 
 # OR
 
 print((lambda x, y: x + y)(5, 7))
+
+# print date in ISO format
+time = lambda: datetime.datetime.now().isoformat()
+print(time())
